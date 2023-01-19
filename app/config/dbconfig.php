aca032127294e1d75e7f46f766a74eb6fa7f44ab
SemPlaatsman <semplaatsman@gmail.com>
2023-01-19 23:14:13 +0100

    Update dbconfig.php

--- app/config/dbconfig.php
+++ app/config/dbconfig.php
@@ -5,7 +5,8 @@ if ($_SERVER['HTTP_HOST'] != 'localhost') {
     $username = "id17574216_developer";
     $password = "4ipzvz{SIy0vBkgX";
     $database = "id17574216_libraryd";
-} else {
+}
+else {
     $type = "mysql";
     $servername = "mysql";
     $username = "developer";
